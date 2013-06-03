5df777b384374da3187ee0c31bcd3831ecbb8207
Giovambattista Fazioli <g.fazioli@wpxtre.me>
2013-06-03 23:26:31 +0200

    Cleanup

--- classes/core/wpdk-shortcode.php
+++ classes/core/wpdk-shortcode.php
@@ -12,50 +12,53 @@
  * @class              WPDKShortcode
  * @author             =undo= <info@wpxtre.me>
  * @copyright          Copyright (C) 2012-2013 wpXtreme Inc. All Rights Reserved.
- * @date               2012-11-28
- * @version            0.1
- * @since              0.9
+ * @date               2013-06-04
+ * @version            0.2.0
+ * @since              0.9.0
  *
  */
 
 class WPDKShortcode {
 
-    /**
-     * Create an instance of WPDKShortcode class
-     *
-     * @brief Construct
-     *
-     * @return WPDKShortcode
-     */
-    public function __construct() {
-        $this->registerShortcodes();
-    }
+  /**
+   * Create an instance of WPDKShortcode class
+   *
+   * @brief Construct
+   *
+   * @return WPDKShortcode
+   */
+  public function __construct()
+  {
+    $this->registerShortcodes();
+  }
 
-    /**
-     * Register all shorcode in shortcodes() array
-     *
-     * @brief Register shortcode
-     */
-    public function registerShortcodes() {
-        $shortcodes = $this->shortcodes();
-        foreach ( $shortcodes as $shortcode => $to_register ) {
-            if ( $to_register ) {
-                add_shortcode( $shortcode, array( $this, $shortcode ) );
-            }
-        }
+  /**
+   * Register all shorcode in shortcodes() array
+   *
+   * @brief Register shortcode
+   */
+  public function registerShortcodes()
+  {
+    $shortcodes = $this->shortcodes();
+    foreach ( $shortcodes as $shortcode => $to_register ) {
+      if ( $to_register ) {
+        add_shortcode( $shortcode, array( $this, $shortcode ) );
+      }
     }
+  }
 
-    /**
-     * Return a Key value pairs array with key as shortcode name and value TRUE/FALSE for turn on/off the shortcode.
-     *
-     * @brief List of allowed shorcode
-     *
-     * @return array Shortcode array
-     */
-    protected function shortcodes() {
-        /* To override. */
-        return array();
-    }
+  /**
+   * Return a Key value pairs array with key as shortcode name and value TRUE/FALSE for turn on/off the shortcode.
+   *
+   * @brief List of allowed shorcode
+   *
+   * @return array Shortcode array
+   */
+  protected function shortcodes()
+  {
+    /* To override. */
+    return array();
+  }
 
 } // class WPDKShortcode
 
