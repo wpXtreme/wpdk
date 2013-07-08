7ef092ab3a75ecf7788dca44a9a4062a14d7a849
Giovambattista Fazioli <g.fazioli@wpxtre.me>
2013-07-08 14:11:08 +0200

    Updated v1.1.2

---
+++ documentation/html/dir_b4fccc93b3ca0a9cc6fe427e6c2fada5.js
@@ -0,0 +1,4 @@
+var dir_b4fccc93b3ca0a9cc6fe427e6c2fada5 =
+[
+    [ "wpdk-terms.php", "wpdk-terms_8php.html", null ]
+];
---
+++ documentation/html/search/pages_77.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['wpdk_20how_2dtos',['WPDK How-tos',['../page_howto.html',1,'']]]
+];
