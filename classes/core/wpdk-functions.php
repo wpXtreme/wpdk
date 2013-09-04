6310c2612789fc9c0b2f7f7c58b9144f817f8f2a
Giovambattista Fazioli <g.fazioli@wpxtre.me>
2013-09-04 11:17:44 +0200

    Cleanup code

--- classes/core/wpdk-functions.php
+++ classes/core/wpdk-functions.php
@@ -196,6 +196,7 @@ function wpdk_checked( $haystack, $current, $echo = true )
 function wpdk_selected( $haystack, $current, $echo = true )
 {
   _deprecated_function( __FUNCTION__, '1.2.0', 'WPDKHTMLTagSelect::selected()' );
+
   if ( is_array( $haystack ) && in_array( $current, $haystack ) ) {
     $current = $haystack = 1;
   }
