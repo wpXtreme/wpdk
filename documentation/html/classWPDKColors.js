a37364fdcf185acfea54419fec6e0b87ef5f1e9b
Giovambattista Fazioli <g.fazioli@wpxtre.me>
2013-09-17 09:46:31 +0200

    Updated 1.2.0

---
+++ documentation/html/classWPDKColors.js
@@ -0,0 +1,5 @@
+var classWPDKColors =
+[
+    [ "hexAddition", "classWPDKColors.html#a3572c272f9a1a710fabcceee837f3395", null ],
+    [ "rgb2hex", "classWPDKColors.html#aeef9c72fa6eec56bfa5c8c28473dc978", null ]
+];
---
+++ documentation/html/search/variables_65.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['error_5fmalformed_5ffile',['ERROR_MALFORMED_FILE',['../classWPDKPreferencesImportExport.html#a3c8996cbaa839fd442204ab787e31497',1,'WPDKPreferencesImportExport']]],
+  ['error_5fnone',['ERROR_NONE',['../classWPDKPreferencesImportExport.html#ab8d7f55fe8c4aaa304f999f29073f09a',1,'WPDKPreferencesImportExport']]],
+  ['error_5fread_5ffile',['ERROR_READ_FILE',['../classWPDKPreferencesImportExport.html#a4cfd5ff929d3c1bbd7ea268a6c0f74f6',1,'WPDKPreferencesImportExport']]],
+  ['error_5fuser_5fid',['ERROR_USER_ID',['../classWPDKPreferencesImportExport.html#aee211f9e76511095d0f6b0a360427738',1,'WPDKPreferencesImportExport']]],
+  ['error_5fversion',['ERROR_VERSION',['../classWPDKPreferencesImportExport.html#a76949fb9dc39d5447f0196e177a76445',1,'WPDKPreferencesImportExport']]]
+];
