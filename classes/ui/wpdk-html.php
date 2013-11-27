1bd5c668b090e838d57c89e12b753fe4080897aa
Giovambattista Fazioli <g.fazioli@wpxtre.me>
2013-11-27 13:34:57 +0100

    Improved endCSSCompress()

--- classes/ui/wpdk-html.php
+++ classes/ui/wpdk-html.php
@@ -38,28 +38,62 @@ class WPDKHTML extends WPDKObject {
    */
   public static function endCSSCompress()
   {
-    $content = ob_get_contents();
+    $str = ob_get_contents();
     ob_end_clean();
 
-    /* Remove comments */
-    $content = preg_replace( '!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $content );
+    $re1 = <<<'EOS'
+(?sx)
+  # quotes
+  (
+    "(?:[^"\\]++|\\.)*+"
+  | '(?:[^'\\]++|\\.)*+'
+  )
+|
+  # comments
+  /\* (?> .*? \*/ )
+EOS;
 
-    $replaces = array(
-      "\r"    => '',
-      " :"    => ':',
-      ": "    => ':',
-      "\n"    => '',
-      "\t"    => '',
-      ";}"    => '}',
-      '  '    => '',
-      '    '  => '',
-      '     ' => '',
-    );
+    $re2 = <<<'EOS'
+(?six)
+  # quotes
+  (
+    "(?:[^"\\]++|\\.)*+"
+  | '(?:[^'\\]++|\\.)*+'
+  )
+|
+  # ; before } (and the spaces after it while we're here)
+  \s*+ ; \s*+ ( } ) \s*+
+|
+  # all spaces around meta chars/operators
+  \s*+ ( [*$~^|]?+= | [{};,>~+-] | !important\b ) \s*+
+|
+  # spaces right of ( [ :
+  ( [[(:] ) \s++
+|
+  # spaces left of ) ]
+  \s++ ( [])] )
+|
+  # spaces left (and right) of :
+  \s++ ( : ) \s*+
+  # but not in selectors: not followed by a {
+  (?!
+    (?>
+      [^{}"']++
+    | "(?:[^"\\]++|\\.)*+"
+    | '(?:[^'\\]++|\\.)*+'
+    )*+
+    {
+  )
+|
+  # spaces at beginning/end of string
+  ^ \s++ | \s++ \z
+|
+  # double spaces to single
+  (\s)\s+
+EOS;
 
-    /* Remove tabs, spaces, newlines, etc. */
-    $content = trim( strtr( $content, $replaces ) );
-
-    return $content;
+    $str = preg_replace( "%$re1%", '$1', $str );
+    return preg_replace( "%$re2%", '$1$2$3$4$5$6$7', $str );
   }
 
   /**
