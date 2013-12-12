60cf872e942ba72d941674ce07f342cf4c285828
Giovambattista Fazioli <g.fazioli@wpxtre.me>
2013-12-12 12:00:18 +0100

    Added style for view

--- classes/ui/wpdk-view.php
+++ classes/ui/wpdk-view.php
@@ -55,8 +55,8 @@
  * @class              WPDKView
  * @author             =undo= <info@wpxtre.me>
  * @copyright          Copyright (C) 2012-2013 wpXtreme Inc. All Rights Reserved.
- * @date               2013-10-17
- * @version            1.1.0
+ * @date               2013-11-12
+ * @version            1.1.1
  *
  */
 
@@ -69,7 +69,7 @@ class WPDKView extends WPDKObject {
    *
    * @var string $version
    */
-  public $version = '1.0.1';
+  public $version = '1.1.1';
 
   /**
    * The CSS class or list of classes
@@ -79,6 +79,15 @@ class WPDKView extends WPDKObject {
    * @var array $class
    */
   public $class;
+
+  /**
+   * Inline style
+   *
+   * @brief Inline style
+   *
+   * @var array $style
+   */
+  public $style;
 
   /**
    * The HTML markup content of this view
@@ -206,9 +215,11 @@ class WPDKView extends WPDKObject {
   public function display()
   {
     $classes = WPDKHTMLTag::classInline( $this->class );
+    $style   = WPDKHTMLTag::styleInline( $this->style );
     $data    = WPDKHTMLTag::dataInline( $this->data );
     ?>
     <div data-type="wpdk-view"
+         style="<?php echo $style ?>"
          id="<?php echo $this->id ?>"
          class="<?php echo $classes ?>" <?php echo $data ?> >
 
