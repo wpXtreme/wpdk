9e9b810b57d81dc46c624111d02c450c2ba01d82
Giovambattista Fazioli <g.fazioli@wpxtre.me>
2014-01-25 14:10:12 +0100

    Double trigger

--- assets/js/wpdk.js
+++ assets/js/wpdk.js
@@ -1874,7 +1874,7 @@ jQuery( function ( $ )
       {
         $( document ).on( 'click', 'span.wpdk-form-clear-left', false, function ()
         {
-          $( this ).prev().val( '' ).triggerHandler( 'change' );
+          $( this ).prev().val( '' ).trigger( 'change' ).trigger( 'keyup' );
         } );
       }
 
