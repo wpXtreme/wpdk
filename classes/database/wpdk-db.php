82e5ad68263c35c7684c3c7402e2fa2cdbb5064e
Giovambattista Fazioli <g.fazioli@wpxtre.me>
2014-03-27 10:42:18 +0100

    Introducing WPDKDBListTableModel

--- classes/database/wpdk-db.php
+++ classes/database/wpdk-db.php
@@ -461,6 +461,7 @@ SQL;
  * @copyright       Copyright (C) 2012-2014 wpXtreme Inc. All Rights Reserved.
  * @date            2014-03-02
  * @version         1.0.0
+ * @deprecated      since 1.5.2 - use WPDKDBListTableModel instead
  *
  */
 class WPDKDBTableModelListTable extends WPDKDBTableModel {
@@ -893,6 +894,241 @@ SQL;
 
 }
 
+/**
+ * Similar to WPDKListTableModel but some useful init and methods for Database
+ *
+ * @class           WPDKDBListTableModel
+ * @author          =undo= <info@wpxtre.me>
+ * @copyright       Copyright (C) 2012-2014 wpXtreme Inc. All Rights Reserved.
+ * @date            2014-03-26
+ * @version         1.0.0
+ *
+ */
+class WPDKDBListTableModel extends WPDKListTableModel {
+
+  // Common Actions
+  const ACTION_NEW     = 'new';
+  const ACTION_INSERT  = 'insert';
+  const ACTION_UPDATE  = 'update';
+  const ACTION_EDIT    = 'action_edit';
+  const ACTION_DELETE  = 'action_delete';
+  const ACTION_DRAFT   = 'action_draft';
+  const ACTION_TRASH   = 'action_trash';
+  const ACTION_RESTORE = 'action_restore';
+
+  public $table;
+
+  /**
+   * Create an instance of WPDKDBListTableModel class
+   *
+   * @brief Construct
+   *
+   * @param string $table_name The name of the database table without WordPress prefix
+   * @param string $sql_file Optional. The filename of SQL file with the database table structure and init data.
+   *
+   * @return WPDKDBListTableModel
+   */
+  public function __construct( $table_name, $sql_file = '' )
+  {
+    // Init parent
+    parent::__construct();
+
+    // Init the table model
+    $this->table = new WPDKDBTableModel( $table_name, $sql_file );
+  }
+
+  // -------------------------------------------------------------------------------------------------------------------
+  // CRUD
+  // -------------------------------------------------------------------------------------------------------------------
+
+  /**
+   * Insert a record by values. Return FALSE if error or id of record if successfully.
+   *
+   * @brief Insert
+   *
+   * @internal string $prefix A prefix used for filter/action hook, eg: carrier, stat, ...
+   * @internal array  $values Array keys values
+   * @internal array  $format Optional. Array keys values for format null values
+   *
+   * @return int|bool
+   */
+  public function insert()
+  {
+    global $wpdb;
+
+    /*
+     * since 1.5.1
+     * try to avoid 'PHP Strict Standards:  Declaration of ::insert() should be compatible with WPDKDBTableModelListTable::insert'
+     *
+     * Remeber that if a params is missing it is NULL
+     */
+    $args = func_get_args();
+    list( $prefix, $values ) = $args;
+    $format = isset( $args[2] ) ? $args[2] : array();
+
+    // Filtrable
+    $values = apply_filters( $prefix . '_insert_values', $values );
+
+    // Insert
+    $result = $wpdb->insert( $this->table_name, $values, $format );
+
+    // Action hook
+    do_action( $prefix . '_inserted', $result, $values );
+
+    if ( false == $result ) {
+      return false;
+    }
+
+    // Get the id
+    return $wpdb->insert_id;
+  }
+
+  /**
+   * Update a record by values. Return FALSE if error or the $where condiction if successfully.
+   * You can use the $where condiction returned to get again the record ID.
+   *
+   * @brief Update
+   *
+   * @internal string $prefix A prefix used for filter/action hook, eg: carrier, stat, ...
+   * @internal array  $values Array keys values
+   * @internal array  $where  Array keys values for where update
+   * @internal array  $format Optional. Array keys values for format null values
+   *
+   * @return array|bool
+   */
+  public function update()
+  {
+    global $wpdb;
+
+    /*
+     * since 1.5.1
+     * try to avoid 'PHP Strict Standards:  Declaration of ::update() should be compatible with WPDKDBTableModelListTable::update'
+     *
+     * Remeber that if a params is missing it is NULL
+     */
+    $args = func_get_args();
+    list( $prefix, $values, $where ) = $args;
+    $format = isset( $args[3] ) ? $args[3] : array();
+
+    // Filtrable
+    $values = apply_filters( $prefix . '_update_values', $values );
+
+    // Update
+    $result = $wpdb->update( $this->table_name, $values, $where, $format );
+
+    // Action hook
+    do_action( $prefix . '_updated', $result, $values, $where );
+
+    if ( false == $result ) {
+      return false;
+    }
+
+    // Get the id
+    return $where;
+  }
+
+  /**
+   * Return the integer count of all rows when $distinct param is emmpty or an array of distinct count for $distinct column.
+   *
+   * @brief    Count
+   *
+   * @internal string       $distinct Optional. Name of field to distinct group by
+   * @internal array|string $status   Optional. Key value paier for where condiction on field: key = fields, vallue = value
+   *
+   * @return int|array
+   */
+  public function count()
+  {
+    global $wpdb;
+
+    /*
+     * since 1.5.1
+     * try to avoid 'PHP Strict Standards:  Declaration of [...] should be compatible with [...]
+     *
+     * Remeber that if a params is missing it is NULL
+     */
+    $args     = func_get_args();
+    $distinct = isset( $args[0] ) ? $args[0] : '';
+    $status   = isset( $args[1] ) ? $args[1] : '';
+
+    $where = '';
+    if ( !empty( $status ) && is_array( $status ) ) {
+      if ( is_numeric( $status[ key( $status ) ] ) ) {
+        $where = sprintf( 'WHERE %s = %s', key( $status ), $status[ key( $status ) ] );
+      }
+      else {
+        $where = sprintf( "WHERE %s = '%s'", key( $status ), $status[ key( $status ) ] );
+      }
+    }
+
+    if ( empty( $distinct ) ) {
+      $sql = <<< SQL
+SELECT COUNT(*) AS count
+  FROM {$this->table_name}
+  {$where}
+SQL;
+
+      return absint( $wpdb->get_var( $sql ) );
+    }
+    else {
+      $sql = <<< SQL
+SELECT DISTINCT( {$distinct} ),
+  COUNT(*) AS count
+  FROM {$this->table_name}
+
+  {$where}
+
+  GROUP BY {$distinct}
+SQL;
+
+      $results = $wpdb->get_results( $sql, ARRAY_A );
+      $result  = array();
+      foreach ( $results as $res ) {
+        $result[ $res[ $distinct ] ] = $res['count'];
+      }
+
+      return $result;
+    }
+  }
+
+  // -------------------------------------------------------------------------------------------------------------------
+  // UTILITIES
+  // -------------------------------------------------------------------------------------------------------------------
+
+  /**
+   * Set one or more record wit a status
+   *
+   * @brief Set a status
+   *
+   * @param int    $id     Record ID
+   * @param string $status Optional. The status, default WPDKDBTableRowStatuses::PUBLISH
+   *
+   * @return mixed
+   */
+  public function status( $id, $status = WPDKDBTableRowStatuses::PUBLISH )
+  {
+    global $wpdb;
+
+    // Stability
+    if ( !empty( $id ) && !empty( $status ) ) {
+
+      // Get the ID
+      $id = implode( ',', (array)$id );
+
+      $sql = <<< SQL
+UPDATE {$this->table_name}
+SET status = '{$status}'
+WHERE id IN( {$id} )
+SQL;
+
+      $num_rows = $wpdb->query( $sql );
+
+      return $num_rows;
+    }
+    return false;
+  }
+
+}
 
 
 
